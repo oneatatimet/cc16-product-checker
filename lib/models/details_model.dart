582be8020939f905c112504bf226b509e2f84f14
Nada <olayan.nada@nagoya-u.jp>
2020-12-24 11:50:08 +0100

    added product details and details model to list products

---
+++ lib/models/details_model.dart
@@ -0,0 +1,8 @@
+class Detials_product{
+  String code;
+  String productName;
+  String ingredientsTextEn;
+  String imageUrl;
+  Detials_product(this.code,this.productName,this.ingredientsTextEn,this.imageUrl);
+
+}
