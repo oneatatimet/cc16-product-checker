86debc2503d181c6f7c3bc3d6b68a32721bb92e5
Nada <olayan.nada@nagoya-u.jp>
2020-12-24 17:17:19 +0100

    updated shared prefrences in main and updated scan results for product list

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:cc16_product_checker/data_store.dart';
 import 'screens/Scanner.dart';
 import 'package:cc16_product_checker/router.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var email = prefs.getString('check');
+  if(email=='yes'){
+    runApp(MyApp());
+  }
+  else{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('check', 'yes');
+    Json_Datafetch obj=new Json_Datafetch();
+    obj.filecreators();
+    runApp(MyApp());
+  }
 }
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Diet Tracker',
+      title: 'Product Checker',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        primaryColor: Color(0xff140257),
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
           backgroundColor: Colors.white,
+
       ),
       home: Scanner(),
       onGenerateRoute: generateRoute,
+
     );
   }
 }
+
 
--- lib/screens/Scan_Results.dart
+++ lib/screens/Scan_Results.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:cc16_product_checker/data_store.dart';
 import 'package:cc16_product_checker/models/APImodel.dart';
+import 'package:cc16_product_checker/models/details_model.dart';
 import 'package:cc16_product_checker/net/api.dart';
-
-// ignore: must_be_immutable
 class ScanResults extends StatefulWidget {
   String product;
   @override
@@ -11,22 +12,22 @@ class ScanResults extends StatefulWidget {
 }
 
 class _ScanResultsState extends State<ScanResults> {
-  MyApi api = new MyApi();
+  MyApi api=new MyApi();
   Future<ProductDetails> myproduct;
   @override
   void initState() {
-    myproduct = api.fetchproducts(widget.product);
+    myproduct=api.fetchproducts(widget.product);
+    // TODO: implement initState
     super.initState();
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color(0xff140257),
-          title: Text("Product Details"),
-        ),
-        body: new FutureBuilder<ProductDetails>(
+      appBar: AppBar(
+        backgroundColor:Color(0xff140257),
+        title: Text("Product Details"),
+      ),
+      body: new FutureBuilder<ProductDetails>(
         future: myproduct, // a Future<String> or null
         builder: (BuildContext context, AsyncSnapshot<ProductDetails> snapshot) {
           switch (snapshot.connectionState) {
@@ -49,20 +50,35 @@ class _ScanResultsState extends State<ScanResults> {
                         children: [
                           Center(
                             child: Container(
-                            
+                            child: Text( snapshot.data.product.productName,
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.w800,
+                              fontFamily: "Poppins",
+                            ),),
                       ),
                           ),
                           SizedBox(height: 15,),
                           Center(
-                            child: Container(
+                            child: snapshot.data.product.imageUrl!=null?Container(
                               height: 200,
                               width: 200,
                               decoration: BoxDecoration(
-                                borderRadius: BorderRadius.all(Radius.circular(10)),
-                                image: DecorationImage(
-                                  image: NetworkImage(snapshot.data.product.imageUrl),
-                                  fit: BoxFit.fill
-                                )
+                                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                                  image: DecorationImage(
+                                      image: NetworkImage(snapshot.data.product.imageUrl),
+                                      fit: BoxFit.fill
+                                  )
+                              ),
+                            ):Container(
+                              height: 200,
+                              width: 200,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                                  image: DecorationImage(
+                                      image: AssetImage("assets/notfound.png"),
+                                      fit: BoxFit.fill
+                                  )
                               ),
                             ),
                           ),
@@ -102,21 +118,127 @@ class _ScanResultsState extends State<ScanResults> {
                                 ),),
                               ),
                             ],
-                            ),
-                           SizedBox(height: 20,),
+                          ),
+                          SizedBox(height: 20,),
                           Container(
                             height: 50,
                             width: MediaQuery.of(context).size.width,
-                            child: RaisedButton(
+                            child: snapshot.data.status==1?RaisedButton(
                               shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(5.0),
                                   side: BorderSide(color: Color(0xff140257))),
-                              onPressed: () {},
+                              onPressed: () {
+                                Json_Datafetch json=new Json_Datafetch();
+                                if(snapshot.data.product.productName==null){
+                                  snapshot.data.product.productName= "No Name Found";
+                                }
+                                if(snapshot.data.product.imageUrl==null){
+                                  snapshot.data.product.productName= "No Image Found";
+                                }
+                                if(snapshot.data.product.ingredientsTextEn==null){
+                                  snapshot.data.product.productName= "No Ingrediants Found";
+                                }
+                                json.add_spending_data(snapshot.data.code, snapshot.data.product.productName, snapshot.data.product.imageUrl,
+                                  snapshot.data.product.ingredientsTextEn,
+                                );
+
+                                showGeneralDialog(
+                                  barrierLabel:"Error",
+                                  barrierDismissible: false,
+                                  barrierColor: Colors.black.withOpacity(0.5),
+                                  transitionDuration: Duration(milliseconds: 500),
+                                  context: context,
+                                  pageBuilder: (_, __, ___) {
+                                    return Align(
+                                      alignment: Alignment.center,
+                                      child: Material(
+                                        color: Colors.transparent,
+                                        child: Container(
+                                          height:270,
+                                          width: 270,
+                                          child: Padding(
+                                            padding: const EdgeInsets.only(top: 40,bottom: 20,left: 30,right: 30),
+                                            child: Column(
+                                              mainAxisAlignment: MainAxisAlignment.start,
+                                              children: [
+                                                Container(
+                                                    height:50,
+                                                    width: 50,
+                                                    child: SvgPicture.asset("assets/check.svg")),
+                                                SizedBox(height: 20,),
+                                                Text(
+                                                  'Product Added!',
+                                                  style: TextStyle(
+                                                      fontSize: 20.0,
+                                                      fontFamily: "Gilroy",
+                                                      color: Theme.of(context).primaryColor,
+                                                      fontWeight: FontWeight.w400),
+                                                ),
+                                                SizedBox(height: 20,),
+                                                Container(
+                                                  height: 35,
+                                                  width: MediaQuery.of(context).size.width,
+                                                  child: RaisedButton(
+                                                    shape: RoundedRectangleBorder(
+                                                        borderRadius: BorderRadius.circular(05.0),
+                                                        side: BorderSide(color: Theme.of(context).primaryColor)),
+                                                    onPressed: () {
+                                                      Navigator.pop(context);
+                                                      Navigator.pop(context);
+                                                    },
+                                                    color: Theme.of(context).primaryColor,
+                                                    textColor: Colors.white,
+                                                    child: Text("View List".toUpperCase(),
+                                                        style: TextStyle(fontSize: 14)),
+                                                  ),
+                                                ),
+                                                SizedBox(height: 10,),
+                                                Container(
+                                                  height: 35,
+                                                  width: MediaQuery.of(context).size.width,
+                                                  child: RaisedButton(
+                                                    shape: RoundedRectangleBorder(
+                                                        borderRadius: BorderRadius.circular(05.0),
+                                                        side: BorderSide(color: Theme.of(context).primaryColor)),
+                                                    onPressed: () {
+                                                      Navigator.pop(context);
+                                                      Navigator.pop(context);
+                                                    },
+                                                    color: Theme.of(context).primaryColor,
+                                                    textColor: Colors.white,
+                                                    child: Text("Back to Home".toUpperCase(),
+                                                        style: TextStyle(fontSize: 14)),
+                                                  ),
+                                                ),
+
+                                              ],
+                                            ),
+                                          ),
+                                          margin: EdgeInsets.only(bottom: 20, left: 12, right: 12,top: 40),
+                                          decoration: BoxDecoration(
+                                            color: Colors.white,
+                                            borderRadius: BorderRadius.circular(10),
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                  transitionBuilder: (_, anim, __, child) {
+                                    return SlideTransition(
+                                      position: Tween(begin: Offset(0, 1), end: Offset(0, 0)).animate(anim),
+                                      child: child,
+                                    );
+                                  },
+                                );
+
+
+
+                              },
                               color: Color(0xff140257),
                               textColor: Colors.white,
                               child: Text("Save Data".toUpperCase(),
                                   style: TextStyle(fontSize: 14)),
-                            ),
+                            ):SizedBox(height: 10,),
                           ),
                           SizedBox(height: 20,),
                           Container(
@@ -135,11 +257,30 @@ class _ScanResultsState extends State<ScanResults> {
                                   style: TextStyle(fontSize: 14)),
                             ),
                           ),
+
+
+
                         ],
                       ),
                     ),
                   ): Container(
-                    child: Text("Not Found"),
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 100,
+                          width: 100,
+                          child: SvgPicture.asset("assets/error.svg"),
+                        ),
+                        SizedBox(height: 20,),
+                        Text("Not Found",style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: "Poppins"
+                        ),),
+                      ],
+                    )
                   ),
 
                 );
